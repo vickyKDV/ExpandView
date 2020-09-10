3bf071adb1609ad1f541a39bc495107c383ab613
cash-it <nibirudeveloper@gmail.com>
2020-09-10 19:19:17 +0700

    Firts Commit

---
+++ ExpandView/src/main/java/com/lesehankoding/expandview/ExpandView.java
@@ -0,0 +1,62 @@
+package com.lesehankoding.expandview;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+
+public class ExpandView extends Animation {
+
+    public static void ExpandInit(View layoutExpand, View arrow) {
+        if (layoutExpand.getVisibility() == View.VISIBLE) {
+            ExpandView.collapse(layoutExpand);
+            arrow.animate().setDuration(100).rotation(0);
+        } else {
+            ExpandView.expandAction(layoutExpand);
+            arrow.animate().setDuration(100).rotation(90);
+        }
+    }
+
+    private static void expandAction(final View view) {
+        view.measure(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        final int actualheight = view.getMeasuredHeight();
+
+        view.getLayoutParams().height = 0;
+        view.setVisibility(View.VISIBLE);
+
+        ExpandView animations = new ExpandView() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                view.getLayoutParams().height = interpolatedTime == 1 ? ViewGroup.LayoutParams.WRAP_CONTENT : (int) (actualheight * interpolatedTime);
+                view.requestLayout();
+            }
+        };
+
+        animations.setDuration((long) (actualheight / view.getContext().getResources().getDisplayMetrics().density));
+        view.startAnimation(animations);
+
+    }
+
+    private static void collapse(final View view) {
+
+        final int actualHeight = view.getMeasuredHeight();
+
+        ExpandView animations = new ExpandView() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+
+                if (interpolatedTime == 1) {
+                    view.setVisibility(View.GONE);
+                } else {
+                    view.getLayoutParams().height = actualHeight - (int) (actualHeight * interpolatedTime);
+                    view.requestLayout();
+
+                }
+            }
+        };
+
+        animations.setDuration((long) (actualHeight/ view.getContext().getResources().getDisplayMetrics().density));
+        view.startAnimation(animations);
+    }
+
+}
---
+++ app/src/main/java/com/lesehankoding/expandanim/MainActivity.java
@@ -0,0 +1,30 @@
+package com.lesehankoding.expandanim;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.lesehankoding.expandview.ExpandView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final TextView textView = findViewById(R.id.textView);
+                ExpandView.ExpandInit(textView,fab);
+            }
+        });
+    }
+
+}
